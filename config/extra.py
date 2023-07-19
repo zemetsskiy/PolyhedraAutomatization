3b135cef30cc14611e22d98a73fef92020f4e054
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-07-19 12:50:18 +0300

    rpcs, scanners, api key info

---
+++ config/extra.py
@@ -0,0 +1,15 @@
+rpcs = {'bsc': 'https://rpc.ankr.com/bsc',
+        'polygon': 'https://polygon-rpc.com',
+        'core': 'https://rpc.coredao.org',
+        'opbnb': 'https://opbnb-testnet-rpc.bnbchain.org',
+        'celo': 'https://rpc.ankr.com/celo'}
+
+scanners = {
+            'bsc': 'https://bscscan.com',
+            'polygon': 'https://polygonscan.com',
+            'core': 'https://scan.coredao.org',
+            'opbnb': 'https://opbnbscan.com',
+            'celo': 'https://celoscan.io'
+            }
+
+API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjkyNTIzNGQ3LTZlYjctNDFiNi1iNjQ1LTk2NmVmNTM1MjdlOSIsIm9yZ0lkIjoiMzQ3NzA3IiwidXNlcklkIjoiMzU3NDA5IiwidHlwZUlkIjoiYjI1MTA1MDMtZDk1Ni00ZDZhLTg3NTgtNTcyYWIwNTcyNDM0IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE2ODkwNjcxMjYsImV4cCI6NDg0NDgyNzEyNn0.sSSjEiOJ1WXTLoQSh8bRUoYRDdLtXJpq4TWkoadod2c"
