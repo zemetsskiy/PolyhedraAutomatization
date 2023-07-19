39a8fcca9e2732c4bccb6c2252d6d11ad568fee3
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-07-19 12:49:56 +0300

    chains info

---
+++ config/chains.py
@@ -0,0 +1,15 @@
+chain_ids = {
+             'bsc': 3,
+             'polygon': 4,
+             'ftm': 6,
+             'core': 17,
+             'mbeam': 9,
+             'nova': 14,
+             'opbnb': 116
+            }
+
+stargate_ids = {
+        'bsc': 102,
+        'polygon': 109,
+        'core': 153
+                }
